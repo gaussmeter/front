d376720a6fb85f2f2b602916a17e559c93dfa1c4
Leland Sindt <leland@sway.org>
2019-03-03 14:40:56 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+  "github.com/gorilla/handlers"
+  "github.com/gorilla/mux"
+  "log"
+  "net/http"
+  "net/http/httputil"
+  "os"
+  "strings"
+)
+
+func main() {
+  proxyBadger := &httputil.ReverseProxy{Director: func(req *http.Request) {
+    originHost := "192.168.1.127:8443"
+    //Todo: change how badger handles secerts, remove this.
+    if strings.Split(req.URL.Path, "/")[1] == "secret" {
+      req.URL.Path = "/badger/" + strings.Split(req.URL.Path,"/")[2]
+
+    }
+    req.Header.Add("X-Forwarded-Host", req.Host)
+    req.Header.Add("X-Origin-Host", originHost)
+    req.Host = originHost
+    req.URL.Scheme = "http"
+    req.URL.Host = originHost
+  }}
+
+  rtr := mux.NewRouter()
+  rtr.HandleFunc("/badger/{*}", func(w http.ResponseWriter, r *http.Request) {
+    proxyBadger.ServeHTTP(w, r)
+  }).Methods("GET","PUT")
+  rtr.HandleFunc("/streamr/{*}", func(w http.ResponseWriter, r *http.Request) {
+    proxyBadger.ServeHTTP(w, r)
+  }).Methods("GET")
+  rtr.HandleFunc("/secret/{*}", func(w http.ResponseWriter, r *http.Request) {
+    proxyBadger.ServeHTTP(w, r)
+  }).Methods("PUT")
+  rtr.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+  loggedRouter := handlers.LoggingHandler(os.Stdout, rtr)
+
+  log.Fatal(http.ListenAndServe(":9001", loggedRouter))
+}
